053ba4cddc2e07962beb26b67694d912d41f864e
DmitryRomaniuk <Kanumowa@gmail.com>
2016-06-27 00:22:13 +0300

    Again work project

--- routes/frontpage.js
+++ routes/frontpage.js
@@ -1,6 +1,6 @@
 var books = require('../models/products.js').books;
 exports.get = function (req, res) {
     var books1=books();
-    console.log(books1);
-    res.render('frontpage', books1);
+    //console.log(books1);
+    res.render('frontpage');
 };
