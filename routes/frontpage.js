2257ff03d4c8fd3d13577ee8fef471b8c6b2c560
Dzmitry Ramaniuk <kanumowa@gmail.com>
2016-06-29 00:07:38 +0300

    Delete JSON.parse

--- routes/frontpage.js
+++ routes/frontpage.js
@@ -2,7 +2,6 @@ var books = require('../models/products.js').books;
 exports.get = function (req, res) {
     books(function (err, data) {
         if (err) return err;
-        data=JSON.parse(data);
         res.render('frontpage', {"booksA":data});
     });
 };
