2a06fb808e99520610301c006977039a7accd59f
DmitryRomaniuk <Kanumowa@gmail.com>
2016-06-30 23:48:41 +0300

    Fix bug in frontpage - covert String to Array

--- routes/frontpage.js
+++ routes/frontpage.js
@@ -2,6 +2,7 @@ var books = require('../models/products.js').books;
 exports.get = function (req, res) {
     books(function (err, data) {
         if (err) return err;
+        if (data) data=JSON.parse(data);
         res.render('frontpage', {"booksA":data});
     });
 };
