719b7ee636fdd2b78206c73ea7b30f708d4bd8ab
DmitryRomaniuk <Kanumowa@gmail.com>
2016-07-02 01:15:58 +0300

    Added parser for url string

---
+++ public/js/frontpage_front.js
@@ -0,0 +1,25 @@
+'use strict';
+function ModelFilter__parseForm() {
+    try {
+        /*$.ajax("/",
+            {type: 'GET', dataType: 'html', success: DataLoaded, error: ErrorHandler}
+        );*/
+        console.log(1)
+    }
+    catch (e) {
+        console.log(e)
+    }
+}
+/*
+function DataLoaded(data)
+{
+    console.log('загруженные через AJAX данные:');
+    console.log(data);
+    document.getElementById('IPlace').innerHTML=data;
+}
+
+function ErrorHandler(jqXHR,StatusStr,ErrorStr)
+{
+    console.log(StatusStr+' '+ErrorStr);
+}
+    */
--- routes/frontpage.js
+++ routes/frontpage.js
@@ -1,8 +1,15 @@
 var books = require('../models/products.js').books;
 exports.get = function (req, res) {
+    var query = req.query;
+    console.log(query.price_from+" "+query.price_to+" "+query.year_from+" "+query.year_to);
     books(function (err, data) {
         if (err) return err;
-        if (data) data=JSON.parse(data);
-        res.render('frontpage', {"booksA":data});
+        if (data) {
+            data = JSON.parse(data)
+        }
+        else {
+            return "error read data from models"
+        }
+        res.render('frontpage', {"booksA": data});
     });
 };
